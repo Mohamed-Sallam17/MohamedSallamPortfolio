bf49e4500642edc3f6498d3826aec6eab639f3b9
unknown <mohamedsallam>
2026-07-08 14:58:41 +0300

    glowcircle

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Footer from "./Footer"
 import Header from "./Header"
+import MouseGlow from "./MouseGlow"
 
 function Layout() {
   return (
     <>
+    <MouseGlow/>
       <Header/>
       <Outlet/>
       <Footer/>
---
+++ src/components/MouseGlow.jsx
@@ -0,0 +1,29 @@
+// src/components/MouseGlow.jsx
+import { useEffect, useState } from "react";
+
+function MouseGlow() {
+  const [mousePos, setMousePos] = useState({ x: -100, y: -100 }); // بدأنا بسالب عشان ما يظهرش فجأة أول ما الصفحة تفتح
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setMousePos({
+        x: e.clientX - 300,
+        y: e.clientY - 300,
+      });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
+  return (
+    <div
+      className="pointer-events-none fixed top-0 left-0 w-[600px] h-[600px] rounded-full opacity-15 mix-blend-screen blur-[100px] transition-transform duration-75 ease-out z-0"
+      style={{
+        background: "radial-gradient(#0b6cc5, rgba(0,0,0,0) 80%)",
+        transform: `translate3d(${mousePos.x}px, ${mousePos.y}px, 0)`,
+      }}
+    />
+  );
+}
+export default MouseGlow
